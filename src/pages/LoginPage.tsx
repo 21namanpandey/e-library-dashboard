b6d26e0713543cb63230dea25a2316b10b1d47c8
Naman Pandey <2492004namanpandey@gmail.com>
2024-07-06 12:49:24 +0530

    add login functionality

--- src/pages/LoginPage.tsx
+++ src/pages/LoginPage.tsx
@@ -9,9 +9,37 @@ import {
 } from "@/components/ui/card";
 import { Input } from "@/components/ui/input";
 import { Label } from "@/components/ui/label";
-import { Link } from "react-router-dom";
+import { login } from "@/http/api";
+import { useMutation } from "@tanstack/react-query";
+import { useRef } from "react";
+import { Link, useNavigate } from "react-router-dom";
 
 const LoginPage = () => {
+  const navigate = useNavigate();
+
+  const emailRef = useRef<HTMLInputElement>(null);
+  const passwordRef = useRef<HTMLInputElement>(null);
+
+  // mutation
+  const mutation = useMutation({
+    mutationFn: login,
+    onSuccess: () => {
+      console.log("login succesfull...");
+      navigate('/dashboard/home')
+    },
+  });
+
+  const handleLoginSubmit = () => {
+    const email = emailRef.current?.value;
+    const password = passwordRef.current?.value;
+
+    if (!email || !password) {
+      return alert("Please enter email and password");
+    }
+
+    mutation.mutate({ email, password });
+  };
+
   return (
     <section className="flex h-screen justify-center items-center ">
       <Card className="w-full max-w-sm">
@@ -25,6 +53,7 @@ const LoginPage = () => {
           <div className="grid gap-2">
             <Label htmlFor="email">Email</Label>
             <Input
+              ref={emailRef}
               id="email"
               type="email"
               placeholder="m@example.com"
@@ -33,12 +62,14 @@ const LoginPage = () => {
           </div>
           <div className="grid gap-2">
             <Label htmlFor="password">Password</Label>
-            <Input id="password" type="password" required />
+            <Input ref={passwordRef} id="password" type="password" required />
           </div>
         </CardContent>
         <CardFooter>
           <div className="w-full">
-            <Button className="w-full">Sign in</Button>
+            <Button onClick={handleLoginSubmit} className="w-full">
+              Sign in
+            </Button>
             <div className="mt-4 text-center text-sm">
               Don't have an account?{" "}
               <Link to={"/auth/register"} className="underline">
