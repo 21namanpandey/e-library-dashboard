7dee7aade8cc98fdf1ab45a5e303de7789e5c2f4
Naman Pandey <2492004namanpandey@gmail.com>
2024-07-07 19:23:00 +0530

    add active link navmenu

--- src/layouts/DashboardLayout.tsx
+++ src/layouts/DashboardLayout.tsx
@@ -1,5 +1,5 @@
 import { Button } from "@/components/ui/button";
-import { Link, Navigate, Outlet } from "react-router-dom";
+import { Link, Navigate, NavLink, Outlet } from "react-router-dom";
 
 import { Badge } from "@/components/ui/badge";
 
@@ -41,7 +41,7 @@ const DashboardLayout = () => {
     return <Navigate to={"/auth/login"} replace />;
   }
   const handleLogout = () => {
-    console.log('loging out ...')
+    console.log("loging out ...");
     setToken("");
   };
   return (
@@ -60,21 +60,29 @@ const DashboardLayout = () => {
           </div>
           <div className="flex-1">
             <nav className="grid items-start px-2 text-sm font-medium lg:px-4">
-              <Link
-                to={"/dashboard/home"}
-                className="flex items-center gap-3 rounded-lg px-3 py-2 text-muted-foreground transition-all hover:text-primary"
+              <NavLink
+                to="/dashboard/home"
+                className={({ isActive }) => {
+                  return `flex items-center gap-3 rounded-lg px-3 py-2 text-muted-foreground transition-all hover:text-primary ${
+                    isActive && "bg-muted"
+                  }`;
+                }}
               >
                 <Home className="h-4 w-4" />
                 Home
-              </Link>
+              </NavLink>
 
-              <Link
-                to={"/dashboard/books"}
-                className="flex items-center gap-3 rounded-lg bg-muted px-3 py-2 text-primary transition-all hover:text-primary"
+              <NavLink
+                to="/dashboard/books"
+                className={({ isActive }) => {
+                  return `flex items-center gap-3 rounded-lg px-3 py-2 text-muted-foreground transition-all hover:text-primary ${
+                    isActive && "bg-muted"
+                  }`;
+                }}
               >
                 <Package className="h-4 w-4" />
                 Books{" "}
-              </Link>
+              </NavLink>
             </nav>
           </div>
           <div className="mt-auto p-4">
